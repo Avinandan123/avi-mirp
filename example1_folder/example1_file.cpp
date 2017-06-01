f209f7af1ca5791930e05bbc831f2dd77af2cc7f
Your Name <you@example.com>
2017-06-01 10:31:06 +0530

    Adding example 1

---
+++ example1_folder/example1_file.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+int a,b,c,d;
+cout << "Enter the cut off marks A:";
+cin >>a;
+cout << "Enter the cut off marks B:";
+cin >>b;
+cout << "Enter the cut off marks c:";
+cin >>c;
+cout << "Enter the obtained number :";
+cin >>d;
+if(a>=100)
+  if(b>=60)
+    if(c>=50)
+      if(d<a)
+        if(d<b)
+          if(d>=c)
+            if(d>=c)
+               cout << "Your grade is C";
+else
+          cout << "You have faild";
+else
+          cout << "Your grade is B";
+else
+          cout << "your grade is A";
+else
+          cout << "Invalid cutoff marks";
+else      
+          cout << "Invalid cutoff marks";
+else 
+          cout << "Invalid cutoff marks";
+          
+          return 0;
+}          
