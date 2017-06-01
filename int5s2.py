65cc16f4c57ddc1a0819871590ee2774a52f216c
Your Name <you@example.com>
2017-06-01 10:08:12 +0530

    educ

---
+++ int5s2.py
@@ -0,0 +1,27 @@
+import math
+a=0.4999
+b=0.0001
+n=2
+while n<1024:
+	n=n*2
+
+	h=(b-a)/n
+
+	def f(x):
+		return 1/math.sqrt(20*(5-x))
+	s=f(a)+f(b)
+	
+	s2=0
+	s1=0
+	
+	for i in range(1,int(n),2):
+		s1=s1+2*f(a+i*h)
+		continue
+	
+	for i in range (2,int(n-1),2):
+		
+		s2=s2+4*f(a+i*h)
+		continue
+		
+	
+	print(n,-4*h*(s+s2+s1)/3)
