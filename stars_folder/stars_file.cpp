806930994704897aa27b013623abaa30ac61066f
Your Name <you@example.com>
2017-06-02 09:38:42 +0530

    Adding stars

---
+++ stars_folder/stars_file.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int N;
+    cout<<"Enter the number:"<<endl;
+    cin>>N;
+    for ( int i=1 ; i<=N; i++) {
+        for (int a=1 ; a<=i; a++) {
+            cout << "*";
+        }
+        cout<< " "<< endl;
+    }
+    return 0;
+}
