9e4d39585efc582b30262c90ff17db4e48c0596e
Your Name <you@example.com>
2017-06-05 11:35:49 +0530

    dj

--- dj_folder/dj_file.cpp
+++ dj_folder/dj_file.cpp
@@ -8,9 +8,15 @@ for (int i=0;i <5 ; i++)
 cout <<x[i]<<" ";
 
 char name[50]="my_name";
-//print using loop
+
+ 
+cout<<name<<endl;
+
 cout<< "Enter first name";
 cin >> name;
+for (int i=0;i<50;i++)
+cout <<name[i]<<" "<<i<<endl;
+cout<<name<<endl;
 
 //print name using loop "Don't use cout<<name"
 
